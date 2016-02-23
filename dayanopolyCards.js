1b0cba1df39a0b2c1bfe8d9b98298ccdee9c5247
headroaster <headroaster@headroaster.com>
2016-02-22 23:31:20 -0800

    Added files via upload

---
+++ dayanopolyCards.js
@@ -0,0 +1,168 @@
+var chanceDeck = [ 
+	card1 = {
+				caption: 'Take a ride on the Reading Railroad.  If you pass go collect $200', 
+				drawn: false, 
+				effect: function (){}
+			},
+	card2 = {
+				caption: 'Bank pays you dividend of $50 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card3 = {
+				caption: 'Advance to Illinois Avenue ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card4 = {
+				caption: 'Your building and loan matures.  Collect $150 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card5 = {
+				caption: 'Get out of jail free card ',
+				 drawn: false,
+				  effect: function (){
+				  }}, 
+	card6 = {
+				caption: 'Make general repairs on all your property.  Pay $25 for each house, Pay $100 for each hotel',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card7 = {
+				caption: 'Advance token to the nearest railroad and pay the owner twice the rental to which he is otherwise entitled. If railroad is unowned, you may buy it from the bank',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card8 = {
+				caption: 'Pay poor tax of $15 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card9 = {
+				caption: 'Take a walk on the Boardwalk ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card10 = {
+				caption: 'Advance to St. Charles Place',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card11 = {
+				caption: 'You have been elected chairman of the board, Pay each player $50',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card12 = {
+				caption: 'Advance token to nearest utility If unowned, you may buy it from the bank If owned, throw the dice and pay owner a total of 10 times the amount thrown',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card13 = {
+				caption: 'Go back 3 spaces',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card14 = {
+				caption: 'Advance to GO! Collect $200 dollars',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card15 = {
+				caption: 'Go directly to jail',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card16 = {
+				caption: 'Advance token to the nearest railroad and pay the owner twice the rental to which he is otherwise entitled. If railroad is unowned, you may buy it from the bank',
+				 drawn: false,
+				  effect: function (){
+				  }}
+    ];
+    
+
+
+
+    var comchestDeck = [ 
+	card1 = {
+				caption: 'Advance to Go (Collect $200) ', 
+				drawn: false, 
+				effect: function (){}
+			},
+	card2 = {
+				caption: 'Bank error in your favor – collect $75 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card3 = {
+				caption: "Doctor's fees – Pay $50 ",
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card4 = {
+				caption: 'Get out of jail free – this card may be kept until needed, or sold ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card5 = {
+				caption: 'Go to jail – go directly to jail – Do not pass Go, do not collect $200 ',
+				 drawn: false,
+				  effect: function (){
+				  }}, 
+	card6 = {
+				caption: 'It is your birthday Collect $10 from each player',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card7 = {
+				caption: 'Grand Opera Night – collect $50 from every player for opening night seats ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card8 = {
+				caption: 'Income Tax refund – collect $20 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card9 = {
+				caption: 'Life Insurance Matures – collect $100 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card10 = {
+				caption: 'Pay Hospital Fees of $100',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card11 = {
+				caption: 'Pay School Fees of $50 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card12 = {
+				caption: 'Receive $25 Consultancy Fee ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card13 = {
+				caption: 'You are assessed for street repairs – $40 per house, $115 per hotel ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card14 = {
+				caption: 'You have won second prize in a beauty contest– collect $10 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card15 = {
+				caption: 'You inherit $100 ',
+				 drawn: false,
+				  effect: function (){
+				  }},
+	card16 = {
+				caption: 'From sale of stock you get $50 ',
+				 drawn: false,
+				  effect: function (){
+				  }}
+    ];
